b1929ac6a3aec1114906fd559b7754ca741a840d
surabhi84ard <surabhipradeep04@gmail.com>
2025-09-05 15:31:47 +0530

    Create code.c

---
+++ c_array_assignment/q1/code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Call by Value: won't affect original variables
+void swapByValue(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Inside swapByValue: a = %d, b = %d\n", a, b);
+}
+
+// Call by Reference: modifies original variables
+void swapByReference(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("Inside swapByReference: a = %d, b = %d\n", *a, *b);
+}
+
+int main() {
+    int x = 10, y = 20;
+
+    printf("Before swapByValue: x = %d, y = %d\n", x, y);
+    swapByValue(x, y);
+    printf("After swapByValue: x = %d, y = %d\n\n", x, y);
+
+    printf("Before swapByReference: x = %d, y = %d\n", x, y);
+    swapByReference(&x, &y);
+    printf("After swapByReference: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
